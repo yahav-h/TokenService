a9d258cf5078a82b5f37e47ce930fe118e9ca699
Yahav-N-Hoffman <yahavh@avanan.com>
2022-06-26 11:12:42 +0300

    fix WebDriverException: unknown error: DevToolsActivePort file doesn't exist while trying to initiate Chrome Browser

--- helpers.py
+++ helpers.py
@@ -24,6 +24,12 @@ def getwebdriver():
     chrome_options = Options()
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument("start-maximized")
+    chrome_options.add_argument("disable-infobars")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.delete_all_cookies()
     return driver
