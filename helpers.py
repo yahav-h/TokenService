4bfa45d04261479b7c104a64b1ddca7e1cbaa4d1
Yahav-N-Hoffman <yahavh@avanan.com>
2022-06-28 11:19:26 +0300

    update helpers.py module - fix DevToolActivePort exception

--- helpers.py
+++ helpers.py
@@ -24,6 +24,14 @@ def getoauth2properties():
 def getwebdriver():
     chrome_options = Options()
     d = DesiredCapabilities.CHROME
+    chrome_options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-setuid-sandbox")
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    chrome_options.add_argument("--disable-dev-shm-using")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("disable-infobars")
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--headless")
     d['loggingPrefs'] = {'browser': 'ALL'}
