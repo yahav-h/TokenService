595c28d6d52bd3667e975932b19cba2a3cf20d91
Yahav-N-Hoffman <yahavh@avanan.com>
2022-07-20 20:44:36 +0300

    update goog_login_page module - finish LOGIN Sequence

--- pages/goog_login_page.py
+++ pages/goog_login_page.py
@@ -4,34 +4,44 @@ class GoogLoginPageLocators:
         EMAIL_INPUT = (By.XPATH, './/input[@type="email"]')
         GENERIC_NEXT_BUTTON = (By.XPATH, './/div[@data-primary-action-label]//button[@type="button"]/span')
         PASSWORD_INPUT = (By.XPATH, './/input[@type="password"]')
+        AUTHORIZATION_CONTINUE_BUTTON = (By.XPATH, './/button/span[contains(text(), "Continue")]')
         AUTHORIZATION_ALLOW_BUTTON = (By.XPATH, './/button/span[contains(text(), "Allow")]')
-        AUTHORIZATION_CANCEL_BUTTON = (By.XPATH, './/button/span[contains(text(), "Cancel")]')
+        GENERIC_ACCESS_CHECKBOXES = (By.XPATH, './/input[@type="checkbox"]')
 
 class GoogLoginPage(BasePage):
-    load_identifier = GoogLoginPageLocators.EMAIL_INPUT
-
     def __init__(self, driver, logger=None):
         super(GoogLoginPage, self).__init__(driver, logger)
+        self.load_identifier = GoogLoginPageLocators.EMAIL_INPUT
 
-    def login(self, email, password):
-        self.log.info("Attempt to do login using : %s , %s" % (email, password))
-        if self.wait_for_element(GoogLoginPageLocators.EMAIL_INPUT):
-            self.set_text(GoogLoginPageLocators.EMAIL_INPUT, email)
-        sleep(3)
-        if self.wait_for_element(GoogLoginPageLocators.GENERIC_NEXT_BUTTON):
-            self.click(GoogLoginPageLocators.GENERIC_NEXT_BUTTON)
-        sleep(3)
-        if self.wait_for_element(GoogLoginPageLocators.PASSWORD_INPUT):
-            self.set_text(GoogLoginPageLocators.PASSWORD_INPUT, password)
-        sleep(3)
-        if self.wait_for_element(GoogLoginPageLocators.GENERIC_NEXT_BUTTON):
-            self.click(GoogLoginPageLocators.GENERIC_NEXT_BUTTON)
-        sleep(3)
-        if (
-                self.wait_for_element(GoogLoginPageLocators.AUTHORIZATION_CANCEL_BUTTON)
-                or
-                self.wait_for_element(GoogLoginPageLocators.AUTHORIZATION_ALLOW_BUTTON)
-            ):
-            self.click(GoogLoginPageLocators.GENERIC_NEXT_BUTTON)
-        sleep(3)
-        return True
+    def login(self, user, password):
+        try:
+            if self.wait_for_element(GoogLoginPageLocators.EMAIL_INPUT):
+                self.set_text(GoogLoginPageLocators.EMAIL_INPUT, user)
+            sleep(3)
+            if self.wait_for_element(GoogLoginPageLocators.GENERIC_NEXT_BUTTON):
+                self.click(GoogLoginPageLocators.GENERIC_NEXT_BUTTON)
+            sleep(3)
+            if self.wait_for_element(GoogLoginPageLocators.PASSWORD_INPUT):
+                self.set_text(GoogLoginPageLocators.PASSWORD_INPUT, password)
+            sleep(3)
+            if self.wait_for_element(GoogLoginPageLocators.GENERIC_NEXT_BUTTON):
+                self.click(GoogLoginPageLocators.GENERIC_NEXT_BUTTON)
+            sleep(3)
+            if self.wait_for_element(GoogLoginPageLocators.AUTHORIZATION_CONTINUE_BUTTON):
+                self.click(GoogLoginPageLocators.AUTHORIZATION_CONTINUE_BUTTON)
+            sleep(3)
+            if self.wait_for_element(GoogLoginPageLocators.GENERIC_ACCESS_CHECKBOXES):
+                for elem in self.get_elements(GoogLoginPageLocators.GENERIC_ACCESS_CHECKBOXES):
+                    elem.click()
+                    sleep(3)
+            sleep(3)
+            if self.wait_for_element(GoogLoginPageLocators.AUTHORIZATION_CONTINUE_BUTTON):
+                self.click(GoogLoginPageLocators.AUTHORIZATION_CONTINUE_BUTTON)
+            sleep(3)
+            if self.wait_for_element(GoogLoginPageLocators.AUTHORIZATION_ALLOW_BUTTON):
+                self.click(GoogLoginPageLocators.AUTHORIZATION_ALLOW_BUTTON)
+            sleep(3)
+        except Exception as e:
+            print(e)
+        url = self._driver.current_url
+        return url
