478da360662a661248531148f92a6c7caaf4f3c8
Yahav-N-Hoffman <yahavh@avanan.com>
2022-07-21 19:26:02 +0300

    test

--- pages/goog_login_page.py
+++ pages/goog_login_page.py
@@ -2,9 +2,9 @@ from pages import BasePage, By, sleep
 
 class GoogLoginPageLocators:
         EMAIL_INPUT = (By.XPATH, './/input[@type="email"]')
-        # GENERIC_NEXT_BUTTON = (By.XPATH, './/div[@data-primary-action-label]//button[@type="button"]/span')
-        GENERIC_NEXT_BUTTON = (By.XPATH, './/input[contains(@value, "Next") or contains(@id, "next")]')
+        EMAIL_NEXT_BUTTON = (By.XPATH, './/input[contains(@value, "Next") or contains(@id, "next")]')
         PASSWORD_INPUT = (By.XPATH, './/input[@type="password"]')
+        PASSWORD_NEXT_BUTTON = (By.XPATH, '')
         AUTHORIZATION_CONTINUE_BUTTON = (By.XPATH, './/button/span[contains(text(), "Continue")]')
         AUTHORIZATION_ALLOW_BUTTON = (By.XPATH, './/button/span[contains(text(), "Allow")]')
         GENERIC_ACCESS_CHECKBOXES = (By.XPATH, './/input[@type="checkbox"]')
@@ -16,48 +16,42 @@ class GoogLoginPage(BasePage):
 
     def login(self, user, password):
         try:
-            self.take_screenshot()
             if self.wait_for_element(GoogLoginPageLocators.EMAIL_INPUT):
                 self.set_text(GoogLoginPageLocators.EMAIL_INPUT, user)
             sleep(5)
 
-            self.take_screenshot()
-            if self.wait_for_element(GoogLoginPageLocators.GENERIC_NEXT_BUTTON):
-                self.click(GoogLoginPageLocators.GENERIC_NEXT_BUTTON)
+            if self.wait_for_element(GoogLoginPageLocators.EMAIL_NEXT_BUTTON):
+                self.click(GoogLoginPageLocators.EMAIL_NEXT_BUTTON)
             sleep(5)
 
-            self.take_screenshot()
             if self.wait_for_element(GoogLoginPageLocators.PASSWORD_INPUT):
                 self.set_text(GoogLoginPageLocators.PASSWORD_INPUT, password)
             sleep(5)
-
-            self.take_screenshot()
-            if self.wait_for_element(GoogLoginPageLocators.GENERIC_NEXT_BUTTON):
-                self.click(GoogLoginPageLocators.GENERIC_NEXT_BUTTON)
+            source = self.get_source_page()
+            with open("new1.html", 'w') as f:
+                f.write(source)
+                f.close()
+            if self.wait_for_element(GoogLoginPageLocators.PASSWORD_NEXT_BUTTON):
+                self.click(GoogLoginPageLocators.PASSWORD_NEXT_BUTTON)
             sleep(5)
 
-            self.take_screenshot()
             if self.wait_for_element(GoogLoginPageLocators.AUTHORIZATION_CONTINUE_BUTTON):
                 self.click(GoogLoginPageLocators.AUTHORIZATION_CONTINUE_BUTTON)
             sleep(5)
 
-            self.take_screenshot()
             if self.wait_for_element(GoogLoginPageLocators.GENERIC_ACCESS_CHECKBOXES):
                 for elem in self.get_elements(GoogLoginPageLocators.GENERIC_ACCESS_CHECKBOXES):
                     elem.click()
                     sleep(5)
             sleep(5)
 
-            self.take_screenshot()
             if self.wait_for_element(GoogLoginPageLocators.AUTHORIZATION_CONTINUE_BUTTON):
                 self.click(GoogLoginPageLocators.AUTHORIZATION_CONTINUE_BUTTON)
             sleep(5)
 
-            self.take_screenshot()
             if self.wait_for_element(GoogLoginPageLocators.AUTHORIZATION_ALLOW_BUTTON):
                 self.click(GoogLoginPageLocators.AUTHORIZATION_ALLOW_BUTTON)
             sleep(5)
-            self.take_screenshot()
         except Exception as e:
             print(e)
         url = self._driver.current_url
