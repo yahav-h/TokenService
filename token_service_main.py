36cdd5996f03dc6eeb19e5ec6b9aafaab50e022b
Yahav-N-Hoffman <yahavh@avanan.com>
2022-07-24 19:04:30 +0300

    add logs

--- token_service_main.py
+++ token_service_main.py
@@ -260,7 +260,7 @@ async def get_record_by_email(email: str):
                 "User": {},
                 "Message": f"User email {email} does not exists!"
             }
-            # logger.debug(f"get_record_by_email | {not_exists_content}")
+            logger.debug(f"get_record_by_email | {not_exists_content}")
             return JSONResponse(content=not_exists_content, status_code=201)
         else:
             dto = TokenUserRecordsDTO(
@@ -278,7 +278,7 @@ async def get_record_by_email(email: str):
                 },
                 "Message": f"User email {email} found!"
             }
-            # logger.debug(f"get_record_by_email | {content}")
+            logger.debug(f"get_record_by_email | {content}")
             return JSONResponse(content=content, status_code=200)
     except Exception as e:
         err_content = {
@@ -287,8 +287,8 @@ async def get_record_by_email(email: str):
             "User": {},
             "Message": "Failed to fetch and / or access data from database"
         }
-        # logger.debug(f"get_record_by_email | {err_content}")
-        # logger.error(e)
+        logger.debug(f"get_record_by_email | {err_content}")
+        logger.error(e)
         return JSONResponse(content=err_content, status_code=404)
 
 
@@ -303,7 +303,7 @@ async def get_record_by_id(uid: int):
                 "User": {},
                 "Message": f"User ID {uid} does not exists!"
             }
-            # logger.debug(f"get_record_by_id | {not_exist_content}")
+            logger.debug(f"get_record_by_id | {not_exist_content}")
             return JSONResponse(content=not_exist_content, status_code=201)
         else:
             dto = TokenUserRecordsDTO(
@@ -321,7 +321,7 @@ async def get_record_by_id(uid: int):
                 },
                 "Message": f"User ID {uid} found!"
             }
-            # logger.debug(f"get_record_by_id | {content}")
+            logger.debug(f"get_record_by_id | {content}")
             return JSONResponse(content=content, status_code=200)
     except Exception as e:
         err_content = {
@@ -330,15 +330,15 @@ async def get_record_by_id(uid: int):
             "User": {},
             "Message": "Failed to fetch and / or access data from database"
         }
-        # logger.debug(f"get_record_by_id | {err_content}")
-        # logger.error(e)
+        logger.debug(f"get_record_by_id | {err_content}")
+        logger.error(e)
         return JSONResponse(content=err_content, status_code=404)
 
 
 @app.post('/users')
 async def add_or_update_user_record_by_email(email: str, oauth: OAuth2Jwt):
     try:
-        # logger.debug(f"email : {email} | oauth : {oauth}")
+        logger.debug(f"email : {email} | oauth : {oauth}")
         pkl_data = pickle.dumps(oauth.to_json())
         dao = TokenUserRecordsDAO.query.filter_by(user=email).first()
         if not dao:
@@ -354,7 +354,7 @@ async def add_or_update_user_record_by_email(email: str, oauth: OAuth2Jwt):
             },
             "Message": f"User email {email} updated!"
         }
-        # logger.debug(f"add_or_update_user_record_by_email | {new_content}")
+        logger.debug(f"add_or_update_user_record_by_email | {new_content}")
         with get_session() as Session:
             Session.add(dao)
         return JSONResponse(content=new_content, status_code=200)
@@ -365,8 +365,8 @@ async def add_or_update_user_record_by_email(email: str, oauth: OAuth2Jwt):
             "User": {},
             "Message": f"Failed to add and / or update data for user {email}"
         }
-        # logger.debug(f"add_or_update_user_record_by_email | {err_content}")
-        # logger.error(e)
+        logger.debug(f"add_or_update_user_record_by_email | {err_content}")
+        logger.error(e)
         return JSONResponse(content=err_content, status_code=404)
 
 if __name__ == '__main__':
