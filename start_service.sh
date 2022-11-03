85c919484f56f2f236c62049a981d808ad63a976
yahav-h <yahavh@avanan.com>
2022-11-03 19:21:29 +0200

    fix start_service.sh script - typo

--- start_service.sh
+++ start_service.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 source ./venv3/bin/activate
-./venv3/bin/python3 -m pip install --upgradable pip && ./venv3/bin/python3 -m pip install -r ./requirements.txt
+./venv3/bin/python3 -m pip install --upgrade pip && ./venv3/bin/python3 -m pip install -r ./requirements.txt
 sudo nohup ./venv3/bin/python3 ./token_service_main.py &
