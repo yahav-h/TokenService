075890152d1733cfec976157b71f2513a2091005
yahav-h <yahavh@avanan.com>
2022-11-06 12:04:39 +0200

    update services cnames

--- helpers.py
+++ helpers.py
@@ -2,33 +2,32 @@ from datetime import datetime
 from hashlib import sha1
 import requests
 
+
+OAUTH_O365_APP_URL = "http://oauth-o365.avanan-dev.net"
+OAUTH_GSUITE_APP_URL = "http://oauth-gsuite.avanan-dev.net"
+
+
 def get_uuid(requester): return sha1(requester.encode()).hexdigest()
 def get_timestamp(): return datetime.now().isoformat()
 
 
 async def request_create_o365_token(email):
-    url = "http://localhost/createToken?email=%s" % email
-    return requests.get(url)
+    return requests.get("http://%s/createToken?email=%s" % (OAUTH_O365_APP_URL, email))
 
 async def request_create_gsuite_token(email):
-    url = "http://localhost/createToken?email=%s" % email
-    return requests.get(url)
+    return requests.get("http://%s/createToken?email=%s" % (OAUTH_GSUITE_APP_URL, email))
 
 async def request_user_o365_token(email):
-    url = "http://localhost/users?email=%s" % email
-    return requests.get(url)
+    return requests.get("http://%s/users?email=%s" % (OAUTH_O365_APP_URL, email))
 
 async def request_user_gsuite_token(email):
-    url = "http://localhost/users?email=%s" % email
-    return requests.get(url)
+    return requests.get("http://%s/users?email=%s" % (OAUTH_GSUITE_APP_URL, email))
 
 async def request_user_o365_token_refresh(email):
-    url = "http://localhost/refreshToken?email=%s" % email
-    return requests.get(url)
+    return requests.get("http://%s/refreshToken?email=%s" % (OAUTH_O365_APP_URL, email))
 
 async def request_user_gsuite_token_refresh(email):
-    url = "http://localhost/refreshToken?email=%s" % email
-    return requests.get(url)
+    return requests.get("http://%s/refreshToken?email=%s" % (OAUTH_GSUITE_APP_URL, email))
 
 async def delegate_action(func, email):
     resp = await func(email)
